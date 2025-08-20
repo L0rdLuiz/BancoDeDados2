ae991dc7135e8be3408c9df13e84e73a12c823cc
Luiz Antonio <101644276+L0rdLuiz@users.noreply.github.com>
2025-08-19 21:52:05 -0300

    Update aula 19-08 BD 2.sql

--- aula 12-08 BD 2.sql
+++ aula 12-08 BD 2.sql
@@ -1,16 +1,22 @@
 CREATE TABLE Funcionario (ID serial primary key, nome text, salario numeric (10,2));
 
-
 CREATE TABLE Funcionario_log (ID serial primary key,id_funcionario int, novo_salario numeric (10,2), data_alteracao TIMESTAMPTZ NOT NULL DEFAULT now());
+
+CREATE TABLE Funcionario_mail (id serial primary key,id_funcionario int, nome text, data_inclusao TIMESTAMPTZ NOT NULL DEFAULT now(), enviado boolean NOT NULL DEFAULT false);
 
 insert into Funcionario (nome, salario)
 values ('saldanha', 1000)
 	   ,('maria', 10000)
 	   ,('jorge', 5000)
+	   
+insert into Funcionario (nome, salario)
+values ('luiz', 3000)
 
-select * from funcionario
+insert into Funcionario (nome, salario)
+values ('isa', 4500)
 
-select * from funcionario_log
+insert into Funcionario (nome, salario)
+values ('carlos', 0)
 
 CREATE OR REPLACE FUNCTION f_log_func() 
 RETURNS TRIGGER AS $$ 
@@ -18,7 +24,7 @@ BEGIN
   INSERT INTO Funcionario_log      (id_funcionario, novo_salario, data_alteracao, antigo_salario) 
   VALUES (new.id, new.salario, now(), old.salario); 
 RETURN NEW; 
-END; $$ LANGUAGE plpgsql; 
+END; $$ LANGUAGE plpgsql;
 
 
 CREATE TRIGGER g_logfunc
@@ -28,9 +34,72 @@ FOR EACH ROW
 WHEN (old.salario<>new.salario)
 EXECUTE FUNCTION f_log_func();
 
+alter table Funcionario_log add column antigo_salario numeric
+
+-- Exercicio 2
+CREATE OR REPLACE FUNCTION f_log_func_email() 
+RETURNS TRIGGER AS $$ 
+BEGIN 
+  INSERT INTO Funcionario_mail     (id_funcionario, nome, data_inclusao, enviado) 
+  VALUES (new.id, new.nome, now(), false); 
+RETURN NEW; 
+END; $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_func_email
+AFTER INSERT ON Funcionario
+FOR EACH ROW
+EXECUTE FUNCTION f_log_func_email();
+-- Fim exercicio 2
+
+-- Exercicio 3
+CREATE OR REPLACE FUNCTION f_log_func_delete() 
+RETURNS TRIGGER AS $$ 
+BEGIN 
+  IF EXISTS (SELECT 1 FROM Funcionario_log where id_funcionario = OLD.id)
+  then RAISE EXCEPTION 'Não é permitido excluir funcionário com alterações de salário registradas no log.';
+  END IF;
+RETURN OLD; 
+END; $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_bloqueia_delete_func
+BEFORE DELETE ON Funcionario
+FOR EACH ROW
+EXECUTE FUNCTION f_log_func_delete();
+
+DELETE FROM Funcionario WHERE id = 11;
+-- Fim exercicio 3
+
+-- Exércicio 4
+
+-- Fim exércicio 4
+
+-- Exércicio 5
+CREATE OR REPLACE FUNCTION f_log_func_insert_null()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.salario IS NULL OR NEW.salario = 0 THEN
+    RAISE EXCEPTION 'Não é permitido inserir salário nulo ou 0.';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_bloqueia_insert_func
+BEFORE INSERT ON Funcionario
+FOR EACH ROW
+EXECUTE FUNCTION f_log_func_insert_null();
+-- Fim exércicio 5
+
+-- Exércicio 6
+
+-- Fim exércicio 6
 update Funcionario set salario = 1500 where id = 1
 
 select fl.id, f.nome, fl.novo_salario, fl.data_alteracao
 from funcionario_log fl inner join funcionario f on (f.id = fl.id_funcionario)
 
-alter table Funcionario_log add column antigo_salario numeric
+select * from funcionario
+
+select * from funcionario_log
+
+select * from funcionario_mail
